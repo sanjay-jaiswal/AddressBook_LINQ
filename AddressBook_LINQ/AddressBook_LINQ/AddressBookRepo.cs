8b1ff54a1879f0520f2ff2b098d1e016a20448a1
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-28 00:57:22 +0530

    [Sanju] ADD : Create a table.

---
+++ AddressBook_LINQ/AddressBook_LINQ/AddressBookRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AddressBook_LINQ
+{
+    public class AddressBookRepo
+    {
+        private readonly DataTable dataTable = new DataTable();
+        public void CreateTable()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(int));
+            dataTable.Columns.Add("Email", typeof(string));
+        }
+    }
+}
---
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============= Welcome To Address Book Using LINQ =================");
+
+            AddressBookRepo addressBook = new AddressBookRepo();
+            addressBook.CreateTable();
+        }
+    }
+}
